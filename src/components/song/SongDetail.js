f2c6f1565f1792c5003c0150e96159da381ea8c0
Santiago <Martnez Bota>
2022-03-29 09:33:39 +0200

    refactorizado el detalle de las canciones en SongDetail

--- src/components/song/SongDetail.js
+++ src/components/song/SongDetail.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { connect } from "react-redux";
 
-const SongDetail = (props) => {
+const SongDetail = ({song}) => {
     
-    const render = props.song === null
+    const render = !song
     ? <div className="text-light"><h1>No hay seleccionada</h1></div> 
-    : <div key={props.song.id.toString()} className="p-5" style={{border: '2px solid white', maxWidth: '30rem'}}><h5 className="p-4 text-center">{props.song.title}</h5><h6 className="text-center">{props.song.duration}</h6></div>
+    : <div key={song.id.toString()} className="p-5" style={{border: '2px solid white', maxWidth: '30rem'}}><h5 className="p-4 text-center">{song.title}</h5><h6 className="text-center">{song.duration}</h6></div>
     
     return (
         <div className="row">
--- src/components/song/SongList.js
+++ src/components/song/SongList.js
@@ -5,7 +5,7 @@ import { selectSong } from "../../actions";
 const SongList = (props) => {
 
     const selected = (song) => {
-        props.dispatch(selectSong(song));
+        props.dispatch(selectSong(song))
     }
 
     const render = props.songs?.map(song => <div onClick={() => selected(song)} key={song.id.toString()} className="m-2" style={{border: '2px solid black'}}><h5 className="p-5 text-center">{song.title}</h5></div>)
